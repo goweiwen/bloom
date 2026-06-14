cff0ce519fec99fbc45e59f96789cce4dcc198e4
Wei Wen Goh <goweiwen@gmail.com>
2026-06-14 19:52:42 +0800

    feat: tooltip follow cursor

--- src/components/tooltip.rs
+++ src/components/tooltip.rs
@@ -2,11 +2,17 @@ use dioxus::prelude::*;
 
 #[component]
 pub fn Tooltip(text: String, children: Element) -> Element {
+    let mut mouse = use_signal(|| (0.0, 0.0));
     rsx! {
-        div { class: "tooltip-container",
+        div {
+            class: "tooltip-container",
+            style: "--mouse-x: {mouse().0}px; --mouse-y: {mouse().1}px;",
+            onmousemove: move |evt| {
+                let coords = evt.element_coordinates();
+                mouse.set((coords.x, coords.y));
+            },
             {children}
-            span { class: "tooltip", { text
-                    } }
+            span { class: "tooltip", {text} }
         }
     }
 }
